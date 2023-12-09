230a7b213a403baa09a2b4d7c4051d05b3bc20cb
Mehdi <mahdi_miyanji@yahoo.com>
2023-12-09 22:32:56 +0330

    change typescript config

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+// https://vitejs.dev/config/
+export default defineConfig({
+    css: {
+        modules: {
+            localsConvention: 'camelCase',
+        },
+    },
+    plugins: [react()],
+});
