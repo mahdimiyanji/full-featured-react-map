0482e7fafef5e8fccb8eb211a7ddba5d19d7d053
Mehdi <mahdi_miyanji@yahoo.com>
2023-12-13 23:40:22 +0330

    add iransans font

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import "./styles.css"
 import MapContainer from "./map/MapContainer.tsx"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      "Iransans",
+      "Roboto"
+    ].join(","),
+    button: {
+      fontFamily: "Iransans"
+    }
+  }
+})
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <MapContainer/>
-    </>
+    </ThemeProvider>
   )
 }
 
